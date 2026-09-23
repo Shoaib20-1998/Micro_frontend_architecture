8208428b3bf132a4a5eedebc7b6207157f020633
shoaib mansoori <shoaib.mansoori@albanero.io>
2026-09-23 17:22:18 +0530

    added empty space for test

--- Single-Spa/app-react-dashboard/src/app-name.js
+++ Single-Spa/app-react-dashboard/src/app-name.js
@@ -15,6 +15,9 @@ const lifecycles = singleSpaReact({
   },
 });
 
+
+
+
 export const bootstrap = lifecycles.bootstrap;
 export const mount = lifecycles.mount;
 export const unmount = lifecycles.unmount;
